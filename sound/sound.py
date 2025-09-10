6f21fc17909c04970d203719d6ada9492b280244
David Jangraw <djangraw@users.noreply.github.com>
2025-09-10 10:29:35 -0400

    Update sound.py
    Converted to int16 format, normalized amplitude, added default fs

--- sound/sound.py
+++ sound/sound.py
@@ -9,14 +9,15 @@ Before using, install playsound:
     conda install conda-forge::playsound
 
 Created on Tue Sep 24 11:55:05 2024
-
 @author: djangraw
+Updated 9/9/25 by DJ - converted to int16 format, normalized amplitude, added default fs.
 """
 
+import numpy as np
 from scipy.io import wavfile 
 from playsound import playsound
 
-def play(x,fs):
+def play(x,fs=44100):
     """
     play a numpy array as a sound.
 
@@ -24,8 +25,8 @@ def play(x,fs):
     ----------
     x : array of floats of size sample_count
         array to play as a sound
-    fs : float
-        sampling rate at which to play the sound
+    fs : float (optional)
+        sampling rate at which to play the sound. The default is 44100.
 
     Returns
     -------
@@ -33,5 +34,11 @@ def play(x,fs):
 
     """
     temp_file = 'TEMP.wav'
+    # rescale the array so we can cast it to type int16
+    amplitude = np.iinfo(np.int16).max
+    x = x/np.abs(x).max()*amplitude
+    x = x.astype(np.int16)
+    # write to file
     wavfile.write(temp_file,fs,x)
+    # play from file
     playsound(temp_file)
